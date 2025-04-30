a8a47b41b4d43fa2bac7f96a1baaae643ce051e7
solaikannan pandiyan <solaikannanpandiyan@gmail.com>
2025-04-30 23:57:44 +0530

    2nd commit

---
+++ DAY 2/code.py
@@ -0,0 +1,37 @@
+# execution flow
+
+# def printing():
+#     print("3")
+#     print("1")
+#     print("2")
+#     print("4")
+
+# if __name__ == '__main__':
+#     print("hello")
+#     printing()
+
+# flow controls
+# 5 days -> basic python
+# pandas, numpy
+# if 
+# elif
+# else
+# break
+# while
+# for
+
+a = 100
+
+if a < 100: 
+    print("a is lesser than 100")
+elif a < 500:
+    print("a is lesser than 500")
+else:
+    print("a is greater than 500") 
+        
+# if a != b: # True or False
+#     print("1")
+#     print("hello")
+    
+# print("4")
+
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # pythonlearning
+
+
+# resources
+
+w3schools -> https://www.w3schools.com/python/default.asp
+
+official -> https://docs.python.org/3/
+
+real python -> https://realpython.com/
